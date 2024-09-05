7e606a0df09778e15bd69aa7eef1166feb8ed225
Md Tarikuzzaman <91418700+tarik2049@users.noreply.github.com>
2024-09-05 19:23:36 +0600

    Innitial Commit
    List introduction, 
    Syntax, 
    Rules List, 
    All List Methods 
    Use case

---
+++ list.py
@@ -0,0 +1,15 @@
+#Example of List
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+#Sequence
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+# #Mutable
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(id(fruits))
+print(fruits)
